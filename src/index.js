b59fb817e654c8be2eb37c95771d1a2df795d86c
Tatiana <138196468+Tanesha001@users.noreply.github.com>
2023-10-06 10:37:53 +0000

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let bracketsObj = {};
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      bracketsObj[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+
+  for (let char of str) {
+      if (bracketsObj[char]) {
+          if (char === bracketsObj[char] && stack[stack.length - 1] === char) {
+              stack.pop();
+          } else {
+              stack.push(char);
+          }
+      } else if (stack.length === 0 || char !== bracketsObj[stack.pop()]) {
+          return false;
+      }
+  }
+
+  return stack.length === 0; 
 }
